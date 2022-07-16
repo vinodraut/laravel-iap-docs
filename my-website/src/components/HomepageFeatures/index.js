974f455036e3c1400baf72e4eb2a026e4e9a0642
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-16 11:13:28 +0300

    update home page features

--- my-website/src/components/HomepageFeatures/index.js
+++ my-website/src/components/HomepageFeatures/index.js
@@ -5,31 +5,31 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/easy-to-use.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <strong>LIAP</strong> was designed from the ground up to be easily installed and
+        used to get your in-app purchases features up and running quickly.
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/focus-on-what-matters.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <strong>LIAP</strong> lets you focus on your logic, and we&apos;ll do the chores. Go
+        ahead and focus on your business requirements not the receipt tasks.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Mock Receipts',
+    Svg: require('@site/static/img/mock-receipts.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <strong>LIAP</strong> gives you the required tools to mock receipts during development.
+        No need to real receipts from different providers.
       </>
     ),
   },
